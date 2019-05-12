40d937f6b20067ebf435c50098726a0250da396c
Aadesh Salecha <salec006@umn.edu>
2019-05-12 15:42:24 -0500

    Cleanup

--- Followers/followers.py
+++ Followers/followers.py
@@ -1,3 +1,5 @@
+import sys
+import shutil
 import time
 import threading
 import csv
@@ -14,175 +16,179 @@ from bs4 import BeautifulSoup
 ###### CONFIG VARIABLES - Changeable Parameters ######
 ######################################################
 
-max_threads = 10
-max_level = 1
-max_retry = 10
-epsilon_follower_diff = 10
+max_threads = 10       # How many simultaneous threads
+max_level = 2          # Depth of graph
+max_retry = 10         # Retries in case of error
+epsilon_diff = 10 
 
+# If you want to resume from a certain level
+# level 0 is input layer
+# Default - Start scraping from layer 1
+start_level = 1
+        
 ######################################################
 ######################################################
 
+path = "result_output/Level"
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
 thread_follower_counts = [0] * max_threads
 all_done = {}
+num_edges = 0
 
 def main():
   global all_done
   global max_threads
   global max_level  
   global thread_follower_counts
-  seen = 0
+  global num_edges
+  global start_level
 
-  if not os.path.exists('LogFiles/'):
-      os.makedirs('LogFiles')
+  if(len(sys.argv) > 1 and sys.argv[1] == "-reset"):
+    reset_folders()    
+
+  if(len(sys.argv) > 2 and sys.argv[2][:len('-start')] == "-start"):
+    start_level = int(sys.argv[2][len('-start'):])
+
+  #########################
+  #### Open Log Files ####
+  make_directory('LogFiles')
 
   tmp_time = str(datetime.datetime.now())
   log_file = open("LogFiles/log_file_" + tmp_time, "w")
-  follower_count_file = open("LogFiles/follower_count_" + tmp_time, "w")
+  log_file_writer = csv.writer(log_file)
+  follower_count_file = open("LogFiles/follower_counts_" + tmp_time, "w")
+  follower_count_writer = csv.writer(follower_count_file)
+  #########################
 
-  for cur_level in range(1, 1+max_level):
-    cur_files = [f for f in listdir("result_output/Level" + str(cur_level-1)) if isfile("result_output/Level" + str(cur_level-1) + "/" + f)]
-    if not os.path.exists('result_output/Level' + str(cur_level)):
-      os.makedirs('result_output/Level' + str(cur_level))
+  for cur_level in range(start_level, 1 + max_level):
+    cur_files = [f for f in listdir(path + str(cur_level-1)) if isfile(path + str(cur_level-1) + "/" + f)]
+    make_directory(path + str(cur_level))
 
     # Generate followers for everyone in cur_files      
     threads = [None] * max_threads  
     for f in cur_files:
-      all_followers = []
-      with open("result_output/Level" + str(cur_level-1) + "/" + f, mode='r') as inptr:
+      # Read in all followers of this file
+      with open(path + str(cur_level-1) + "/" + f, mode='r') as inptr:
         reader = csv.reader(inptr)
-        for row in reader:
-          all_followers.append(row[0])
 
-      follower_index = 0
-      while follower_index < len(all_followers):        
-        follower = all_followers[follower_index]
-        try:
-          # We already have followers then don't recompute
-          if(not(follower in all_done)):                    
-            # if we have space
-            for thread_num in range(max_threads):
-              if(threads[thread_num] == None or not(threads[thread_num].isAlive())):
-                seen += thread_follower_counts[thread_num]
-                thread_follower_counts[thread_num] = 0
+        follower = next_follower(reader) # First follower
+        while True:        
+          try:
+            # We already have followers then don't recompute
+            if(not(follower in all_done)):                    
+              for thread_num in range(max_threads): # if we have space
+                if(threads[thread_num] == None or not(threads[thread_num].isAlive())):
+                  num_edges += thread_follower_counts[thread_num]
+                  thread_follower_counts[thread_num] = 0
 
-                print("\nStart thread for: ", follower, " at ", str(datetime.datetime.now()))
-                threads[thread_num] = threading.Thread(target=generateFollowers, args=(follower, cur_level, log_file, thread_num, follower_count_file))
-                threads[thread_num].start()
-                all_done[follower] = True
+                  threads[thread_num] = threading.Thread(target=generateFollowers, args=(follower, cur_level, thread_num, log_file_writer, follower_count_writer))
+                  threads[thread_num].start()
+                  all_done[follower] = True
 
-                follower_index += 1
-                print("Total nodes processed = ", len(all_done))
-                # print("Total edges seen = ", seen)
-                break
-          else:
-            follower_index += 1
-                
-        except KeyboardInterrupt:
-          print("Total nodes processed = ", len(all_done))
-          log_file.close()
-          follower_count_file.close()
-          sys.exit()
-          # print("Total edges seen = ", seen + sum(thread_follower_counts))
+                  follower = next_follower(reader)
+
+                  print("\nStart thread for: ", follower, " at ", str(datetime.datetime.now()))
+                  print("Total nodes processed = ", len(all_done))
+                  break
+            else:
+              follower = next_follower(reader)
+          
+          except StopIteration:
+            break # We sucessfuly read the whole list
+
+          except KeyboardInterrupt:
+            log_file.close()
+            follower_count_file.close()
+            sys.exit()
 
     for thread_num in range(max_threads):
       if(threads[thread_num] != None):
         threads[thread_num].join()
 
-  print("Total nodes processed = " + str(len(all_done)), file = log_file)
-  print("Total edges seen = " + str(seen), file = log_file)
+  log_file_writer.writerow([]) # Next line
+  log_file_writer.writerow(["Total nodes processed: ", str(len(all_done))])
+  log_file_writer.writerow(["Total edges seen: ", str(num_edges)])
   log_file.close()
   follower_count_file.close()
 
-def generateFollowers(org, level, log_file, thread_num, follower_count_file):
+def generateFollowers(org, level, thread_num, log_file_writer, follower_count_writer):
   global max_retry
-  global epsilon_follower_diff
+  global epsilon_diff
 
   # Open page
   link = "https://mobile.twitter.com/" + org + "/followers"  
-  outptr = open("result_output/Level" + str(level) + "/followers_" + org + ".txt", mode='w', encoding="utf-8")
+  outptr = open(path + str(level) + "/followers_" + org + ".txt", mode='w', encoding="utf-8")
   writer = csv.writer(outptr, dialect='excel', delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)  
+  
   try:
     req = Request(link, headers=headers)
     page = urlopen(req)
     doc = lxml.html.fromstring(page.read())
+  
+  except urllib.error.HTTPError as e:
+    outptr.close()
+    log_file_writer.writerow(["\nUser does not exist anymore: ", org])
+    return 0
 
-    # Extract number of followers for later verification
+  # Extract number of followers for later verification
+  try:
+    num_followers = int(doc.xpath('//*[@id="main_content"]/div/div[1]/table/tr[2]/td/span/text()')[0].replace(',', ''))
+  
+  except:
+    outptr.close()
+    log_file_writer.writerow(["User is protected: ", org])  
+    print("User is protected " + org)
+    return 0
+
+  # Extract first 20 followers
+  followers = doc.xpath('//span[@class="username"]/text()')[1:]
+  num_scraped_followers = len(followers)
+  for follower in followers:
+      writer.writerow([follower, org])
+
+  # Click on Show More and continue till we get all followers
+  error_count = 0
+  while(abs(num_scraped_followers - num_followers) > epsilon_diff and error_count < max_retry):
     try:
-      num_followers = int(doc.xpath('//*[@id="main_content"]/div/div[1]/table/tr[2]/td/span/text()')[0].replace(',', ''))
-    except:
-      print("User is protected: ", org, file = log_file)
-      raise Exception("User is protected " + org)
+      link = "https://mobile.twitter.com/" + doc.xpath('//*[@id="main_content"]/div/div[2]/div/a')[0].get('href')
+    except Exception as e:
+      if(abs(num_scraped_followers - num_followers) < epsilon_diff):
+        break
 
-    # Extract first 20 followers
+      log_file_writer.writerow(["Error: ", e])
+      printPage(page, "Error#" + str(error_count) + "_" + org)
+      error_count += 1
+      time.sleep(1)
+
+    req = Request(link, headers=headers)
+    page = urlopen(req)
+
+    # Make sure we have a good page read
+    while(page.getcode() > 400):
+      print(org, link, page.getcode())
+      time.sleep(1)
+      page = urlopen(req)
+
+    doc = lxml.html.fromstring(page.read())
     followers = doc.xpath('//span[@class="username"]/text()')[1:]
-    num_scraped_followers = len(followers)
+    num_scraped_followers += len(followers)
     for follower in followers:
         writer.writerow([follower, org])
 
-    # Click on Show More and continue till we get all followers
-    error_count = 0
-    while(abs(num_scraped_followers - num_followers) > epsilon_follower_diff and error_count < max_retry):
-      try:
-        link = "https://mobile.twitter.com/" + doc.xpath('//*[@id="main_content"]/div/div[2]/div/a')[0].get('href')
-        req = Request(link, headers=headers)
-        page = urlopen(req)
+  if(abs(num_scraped_followers - num_followers) > epsilon_diff):
+    print("\nUser not fully extracted ", org, num_scraped_followers, num_followers, link)
+    log_file_writer.writerow(["\nUser not fully extracted ", org, num_scraped_followers, num_followers, link])
+    printPage(page, org)
 
-        # Make sure we have a good page read
-        while(page.getcode() > 400):
-          print(org, link, page.getcode())
-          time.sleep(1)
-          page = urlopen(req)
+  thread_follower_counts[thread_num-1] = num_scraped_followers
+  follower_count_writer.writerow([org, str(num_followers), str(num_scraped_followers)])
+  outptr.close()
+  return num_scraped_followers
 
-        # print(page.geturl())
-        doc = lxml.html.fromstring(page.read())
-
-        followers = doc.xpath('//span[@class="username"]/text()')[1:]
-        num_scraped_followers += len(followers)
-        for follower in followers:
-            writer.writerow([follower, org])
-
-      except Exception as e:
-        if(abs(num_scraped_followers - num_followers) < epsilon_follower_diff):
-          break
-
-        print(e, file = log_file)
-        printPage(page, "Error#" + str(error_count) + "_" + org)
-        error_count += 1
-        time.sleep(1)
-
-        ##############
-        # Re-open page
-        ##############
-        page = urlopen(req)
-        while(page.getcode() > 400):
-          print(org, link, page.getcode())
-          time.sleep(1)
-          page = urlopen(req)
-
-        # print(page.geturl())
-        doc = lxml.html.fromstring(page.read())
-
-        followers = doc.xpath('//span[@class="username"]/text()')[1:]
-        num_scraped_followers += len(followers)
-        for follower in followers:
-            writer.writerow([follower, org])
-        pass
-
-    if(abs(num_scraped_followers - num_followers) > epsilon_follower_diff):
-      print("\nUser not fully extracted ", org, num_scraped_followers, num_followers, link)
-      print("\nUser not fully extracted ", org, num_scraped_followers, num_followers, link, file = log_file)
-      printPage(page, org)
-
-    thread_follower_counts[thread_num-1] = num_scraped_followers
-    print(org + ", " + str(num_followers) + ", " + str(num_scraped_followers), file = follower_count_file)
-    outptr.close()
-    return num_scraped_followers
-
-  except urllib.error.HTTPError as e:
-    outptr.close()
-    print("\nUser does not exist anymore: ", org, file = log_file)
-    return 0
+def reset_folders():
+  sub_dirs = [f.path for f in os.scandir("./result_output") if ("Level0" not in f.path and f.is_dir())]
+  for cur_dir in sub_dirs:
+    shutil.rmtree(cur_dir)
 
 def printPage(page, name):
   soup = BeautifulSoup(page.read(), 'lxml')
@@ -191,6 +197,13 @@ def printPage(page, name):
   misc = open("ErrorFiles/" + name + ".html", "w")
   print(soup.prettify(), file = misc)
   misc.close() 
+
+def make_directory(name):
+  if not os.path.exists(name):
+    os.makedirs(name)
+
+def next_follower(reader):
+  return next(reader)[0]
 
 if __name__=="__main__":
   main()
--- Friends/friends.py
+++ Friends/friends.py
@@ -1,3 +1,6 @@
+import sys
+import shutil
+import time
 import threading
 import csv
 from os import listdir
@@ -7,141 +10,202 @@ from urllib.request import urlopen, Request
 import lxml.html
 import datetime
 import os
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
-###### CONFIG VARIABLES - Changeable Parameters
+######################################################
+###### CONFIG VARIABLES - Changeable Parameters ######
+######################################################
 
-max_threads = 5
-max_level = 2
+max_threads = 10       # How many simultaneous threads
+max_level = 1          # Depth of graph
+max_retry = 10         # Retries in case of error
+epsilon_diff = 10 
 
-#######
+# If you want to resume from a certain level
+# level 0 is input layer
+# Default - Start scraping from layer 1
+start_level = 1
+        
+######################################################
+######################################################
 
+path = "result_output/Level"
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
-thread_friend_counts = [0, 0, 0, 0, 0]
+thread_friend_counts = [0] * max_threads
 all_done = {}
+num_edges = 0
 
 def main():
   global all_done
   global max_threads
   global max_level  
   global thread_friend_counts
-  seen = 0
+  global num_edges
+  global start_level
 
-  if not os.path.exists('LogFiles/'):
-      os.makedirs('LogFiles')
+  if(len(sys.argv) > 1 and sys.argv[1] == "-reset"):
+    reset_folders()    
 
-  log_file = open("LogFiles/log_file_" + str(datetime.datetime.now()), "w")
+  if(len(sys.argv) > 2 and sys.argv[2][:len('-start')] == "-start"):
+    start_level = int(sys.argv[2][len('-start'):])
 
-  for cur_level in range(1, 1+max_level):
-    cur_files = [f for f in listdir("result_output/Level" + str(cur_level-1)) if isfile("result_output/Level" + str(cur_level-1) + "/" + f)]
-    if not os.path.exists('result_output/Level' + str(cur_level)):
-      os.makedirs('result_output/Level' + str(cur_level))
+  #########################
+  #### Open Log Files ####
+  make_directory('LogFiles')
 
-    # Generate friends for everyone in cur_files    
-    threads = [None] * max_threads        
+  tmp_time = str(datetime.datetime.now())
+  log_file = open("LogFiles/log_file_" + tmp_time, "w")
+  log_file_writer = csv.writer(log_file)
+  friend_count_file = open("LogFiles/friend_counts_" + tmp_time, "w")
+  friend_count_writer = csv.writer(friend_count_file)
+  #########################
+
+  for cur_level in range(start_level, 1 + max_level):
+    cur_files = [f for f in listdir(path + str(cur_level-1)) if isfile(path + str(cur_level-1) + "/" + f)]
+    make_directory(path + str(cur_level))
+
+    # Generate friends for everyone in cur_files      
+    threads = [None] * max_threads  
     for f in cur_files:
-      all_friends = []
-      with open("result_output/Level" + str(cur_level-1) + "/" + f, mode='r') as inptr:
+      # Read in all friends of this file
+      with open(path + str(cur_level-1) + "/" + f, mode='r') as inptr:
         reader = csv.reader(inptr)
-        for row in reader:
-          # Small change from follower.py to keep input format consistent
-          if(cur_level != 1):
-            all_friends.append(row[1])
-          else:
-            all_friends.append(row[0])
 
-      friend_index = 0
-      while friend_index < len(all_friends):        
-        friend = all_friends[friend_index]
-        try:
-          # We already have friends then don't recompute
-          if(not(friend in all_done)):                    
-            # if we have space
-            for thread_num in range(max_threads):
-              if(threads[thread_num] == None or not(threads[thread_num].isAlive())):
-                seen += thread_friend_counts[thread_num]
-                thread_friend_counts[thread_num] = 0
+        friend = next_friend(reader, cur_level) # First friend
+        while True:        
+          try:
+            # We already have friends then don't recompute
+            if(not(friend in all_done)):                    
+              for thread_num in range(max_threads): # if we have space
+                if(threads[thread_num] == None or not(threads[thread_num].isAlive())):
+                  num_edges += thread_friend_counts[thread_num]
+                  thread_friend_counts[thread_num] = 0
 
-                print("Start ", friend)
-                threads[thread_num] = threading.Thread(target=generateFriends, args=(friend, cur_level, log_file, thread_num))
-                threads[thread_num].start()
-                all_done[friend] = True
+                  threads[thread_num] = threading.Thread(target=generateFriends, args=(friend, cur_level, thread_num, log_file_writer, friend_count_writer))
+                  threads[thread_num].start()
+                  all_done[friend] = True
 
-                friend_index += 1
-                print("Total nodes processed = ", len(all_done))
-                print("Total edges seen = ", seen)
-                break
-          else:
-            friend_index += 1
-                
-        except KeyboardInterrupt:
-          print("Total nodes processed = ", len(all_done))
-          print("Total edges seen = ", seen + sum(thread_friend_counts))
-          raise Exception("Kill")  
+                  friend = next_friend(reader, cur_level)
+
+                  print("\nStart thread for: ", friend, " at ", str(datetime.datetime.now()))
+                  print("Total nodes processed = ", len(all_done))
+                  break
+            else:
+              friend = next_friend(reader, cur_level)
+          
+          except StopIteration:
+            break # We sucessfuly read the whole list
+
+          except KeyboardInterrupt:
+            log_file.close()
+            friend_count_file.close()
+            sys.exit()
 
     for thread_num in range(max_threads):
       if(threads[thread_num] != None):
         threads[thread_num].join()
 
-  print("Total nodes processed = " + str(len(all_done)), file = log_file)
-  print("Total edges seen = " + str(seen), file = log_file)
+  log_file_writer.writerow([]) # Next line
+  log_file_writer.writerow(["Total nodes processed: ", str(len(all_done))])
+  log_file_writer.writerow(["Total edges seen: ", str(num_edges)])
+  log_file.close()
+  friend_count_file.close()
 
-def generateFriends(org, level, log_file, thread_num):
+def generateFriends(org, level, thread_num, log_file_writer, friend_count_writer):
+  global max_retry
+  global epsilon_diff
+
+  # Open page
+  link = "https://mobile.twitter.com/" + org + "/following"  
+  outptr = open(path + str(level) + "/friends_" + org + ".txt", mode='w', encoding="utf-8")
+  writer = csv.writer(outptr, dialect='excel', delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)  
+  
   try:
-    # Open page
-    link = "https://mobile.twitter.com/" + org + "/following"
+    req = Request(link, headers=headers)
+    page = urlopen(req)
+    doc = lxml.html.fromstring(page.read())
+  
+  except urllib.error.HTTPError as e:
+    outptr.close()
+    log_file_writer.writerow(["\nUser does not exist anymore: ", org])
+    return 0
+
+  # Extract number of friends for later verification
+  try:
+    num_friends = int(doc.xpath('//*[@id="main_content"]/div/div[1]/table/tr[2]/td/span/text()')[0].replace(',', ''))
+  
+  except:
+    outptr.close()
+    log_file_writer.writerow(["User is protected: ", org])  
+    print("User is protected " + org)
+    return 0
+
+  # Extract first 20 friends
+  friends = doc.xpath('//span[@class="username"]/text()')[1:]
+  num_scraped_friends = len(friends)
+  for friend in friends:
+      writer.writerow([friend, org])
+
+  # Click on Show More and continue till we get all friends
+  error_count = 0
+  while(abs(num_scraped_friends - num_friends) > epsilon_diff and error_count < max_retry):
+    try:
+      link = "https://mobile.twitter.com/" + doc.xpath('//*[@id="main_content"]/div/div[2]/div/a')[0].get('href')
+    except Exception as e:
+      if(abs(num_scraped_friends - num_friends) < epsilon_diff):
+        break
+
+      log_file_writer.writerow(["Error: ", e])
+      printPage(page, "Error#" + str(error_count) + "_" + org)
+      error_count += 1
+      time.sleep(1)
+
     req = Request(link, headers=headers)
     page = urlopen(req)
 
+    # Make sure we have a good page read
+    while(page.getcode() > 400):
+      print(org, link, page.getcode())
+      time.sleep(1)
+      page = urlopen(req)
+
     doc = lxml.html.fromstring(page.read())
-    # Extract first 20 friends
     friends = doc.xpath('//span[@class="username"]/text()')[1:]
-          
-    # Click on Show More and continue till we get all friends
-    while(True):
-      try:
-        link = "https://mobile.twitter.com/" + doc.xpath('//*[@id="main_content"]/div/div[2]/div/a')[0].get('href')
-        req = Request(link, headers=headers)
-        page = urlopen(req)
-        # print(page.geturl())
-        doc = lxml.html.fromstring(page.read())
-        friends += doc.xpath('//span[@class="username"]/text()')[1:]
-      except:
-        # Exception might mean a) we have reached end or
-        #                      b) some type of timeout
-        # Therefore try once more to make sure we have collected all friends
-        try:
-          link = "https://mobile.twitter.com/" + doc.xpath('//*[@id="main_content"]/div/div[2]/div/a')[0].get('href')
-          req = Request(link, headers=headers)
-          page = urlopen(req)
-          doc = lxml.html.fromstring(page.read())
-          friends += doc.xpath('//span[@class="username"]/text()')[1:]
-        except Exception as e:
-          # If we had exception on the second try
-          # we can be confident that we have all friends
-          break
-        thread_friend_counts[thread_num-1] = len(friends)
+    num_scraped_friends += len(friends)
+    for friend in friends:
+        writer.writerow([friend, org])
 
-    # Write friends to file
-    with open("result_output/Level" + str(level) + "/friends_" + org + ".txt", mode='w', encoding="utf-8") as outptr:
-      writer = csv.writer(outptr, dialect='excel', delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-      for friend in friends:
-        writer.writerow([org, friend])
+  if(abs(num_scraped_friends - num_friends) > epsilon_diff):
+    print("\nUser not fully extracted ", org, num_scraped_friends, num_friends, link)
+    log_file_writer.writerow(["\nUser not fully extracted ", org, num_scraped_friends, num_friends, link])
+    printPage(page, org)
 
-    thread_friend_counts[thread_num-1] = len(friends)
-    return len(friends)
-  except urllib.error.HTTPError as e:
-    print("User does not exist anymore: ", org, file = log_file)
-    return 0        
+  thread_friend_counts[thread_num-1] = num_scraped_friends
+  friend_count_writer.writerow([org, str(num_friends), str(num_scraped_friends)])
+  outptr.close()
+  return num_scraped_friends
 
+def reset_folders():
+  sub_dirs = [f.path for f in os.scandir("./result_output") if ("Level0" not in f.path and f.is_dir())]
+  for cur_dir in sub_dirs:
+    shutil.rmtree(cur_dir)
 
-# def printPage(req):
-#   page = urlopen(req)
-#   soup = BeautifulSoup(page.read(), 'lxml')
-#   misc = open("error.html", "w")
-#   print(soup.prettify(), file = misc)
-#   misc.close() 
+def printPage(page, name):
+  soup = BeautifulSoup(page.read(), 'lxml')
+  if not os.path.exists('ErrorFiles/'):
+    os.makedirs('ErrorFiles')
+  misc = open("ErrorFiles/" + name + ".html", "w")
+  print(soup.prettify(), file = misc)
+  misc.close() 
+
+def make_directory(name):
+  if not os.path.exists(name):
+    os.makedirs(name)
+
+def next_friend(reader, cur_level):
+  if(cur_level != 1):
+    return next(reader)[1]
+  return next(reader)[0]
 
 if __name__=="__main__":
   main()
-
