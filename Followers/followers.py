61a8db9c9b629e27fe5d8970d53358d1adff0af2
Aadesh Salecha <salec006@umn.edu>
2019-05-15 13:34:11 -0500

    Global Parellelism

--- Followers/followers.py
+++ Followers/followers.py
@@ -32,9 +32,12 @@ start_level = 1
 
 path = "result_output/Level"
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
-thread_follower_counts = [0] * max_threads
+
 all_done = {}
 num_edges = 0
+file_queue = queue.Queue()
+threads = [None] * max_threads 
+thread_follower_counts = [0] * max_threads
 
 def main():
   global all_done
@@ -64,55 +67,58 @@ def main():
   for cur_level in range(1, max_level+1):
     make_directory(path + str(cur_level))
 
-  file_queue = queue.SimpleQueue()
-  file_queue.put(0, "input")
+  cur_files = [f for f in listdir("result_output/Level" + str(start_level-1)) if isfile("result_output/Level" + str(start_level-1) + "/" + f)]  
+  for file in cur_files:
+    print(file)
+    file_queue.put((start_level-1, file))
   
-  # for cur_level in range(start_level, 1 + max_level):
-  threads = [None] * max_threads 
-
   # Lock for semaphore
   lock = threading.Lock() 
   while(not file_queue.empty()):
-      # Accessing queue with semaphore
-      lock.acquire()
-      tmp_level, f = file_queue.get()
-      lock.release()
+    # Accessing queue with semaphore
+    lock.acquire()
+    tmp_level, f = file_queue.get()
+    lock.release()
 
-      file_name = path + str(tmp_level-1) + "/" + f
+    file_name = path + str(tmp_level) + "/" + f
+    print(tmp_level, f)
 
-      # Read in all followers of this file
-      with open(file_name, mode='r') as inptr:
-        reader = csv.reader(inptr)
+    # Read in all followers of this file
+    with open(file_name, mode='r') as inptr:
+      reader = csv.reader(inptr)
 
-        follower = next_follower(reader) # First follower
-        while True:        
-          try:
-            # We already have followers then don't recompute
-            if(not(follower in all_done)):                    
-              for thread_num in range(max_threads): # if we have space
-                if(threads[thread_num] == None or not(threads[thread_num].isAlive())):
-                  num_edges += thread_follower_counts[thread_num]
-                  thread_follower_counts[thread_num] = 0
+      follower = next_follower(reader) # First follower
+      while True:        
+        try:
+          # We already have followers then don't recompute
+          if(not(follower in all_done)):                    
+            for thread_num in range(max_threads): # if we have space
+              if(threads[thread_num] == None or not(threads[thread_num].isAlive())):
+                num_edges += thread_follower_counts[thread_num]
+                thread_follower_counts[thread_num] = 0
 
-                  threads[thread_num] = threading.Thread(target=generateFollowers, args=(follower, cur_level, thread_num, log_file_writer, follower_count_writer, file_queue, lock))
-                  threads[thread_num].start()
-                  all_done[follower] = True
+                print("\nStart thread for: ", follower, " at ", str(datetime.datetime.now()))
+                # print("Total nodes processed = ", len(all_done))
 
-                  follower = next_follower(reader)
+                threads[thread_num] = threading.Thread(target=generateFollowers, args=(follower, tmp_level+1, thread_num, log_file_writer, follower_count_writer, lock))
+                threads[thread_num].start()
+                all_done[follower] = True
 
-                  print("\nStart thread for: ", follower, " at ", str(datetime.datetime.now()))
-                  print("Total nodes processed = ", len(all_done))
-                  break
-            else:
-              follower = next_follower(reader)
-          
-          except StopIteration:
-            break # We sucessfuly read the whole list
+                follower = next_follower(reader)
+                break
+          else:
+            follower = next_follower(reader)
+        
+        except StopIteration:
+          break # We sucessfuly read the whole list
 
-          except KeyboardInterrupt:
-            log_file.close()
-            follower_count_file.close()
-            sys.exit()
+        except KeyboardInterrupt:
+          log_file.close()
+          follower_count_file.close()
+          sys.exit()
+
+    while(file_queue.empty() and is_somethread_alive()):
+      time.sleep(1)
 
   for thread_num in range(max_threads):
     if(threads[thread_num] != None):
@@ -124,9 +130,10 @@ def main():
   log_file.close()
   follower_count_file.close()
 
-def generateFollowers(org, level, thread_num, log_file_writer, follower_count_writer, file_queue, lock):
+def generateFollowers(org, level, thread_num, log_file_writer, follower_count_writer, lock):
   global max_retry
   global epsilon_diff
+  global file_queue
 
   # Open page
   link = "https://mobile.twitter.com/" + org + "/followers"  
@@ -157,7 +164,7 @@ def generateFollowers(org, level, thread_num, log_file_writer, follower_count_wr
   followers = doc.xpath('//span[@class="username"]/text()')[1:]
   num_scraped_followers = len(followers)
   for follower in followers:
-    writer.writerow([follower, org])
+      writer.writerow([follower, org])
 
   # Click on Show More and continue till we get all followers
   error_count = 0
@@ -198,9 +205,10 @@ def generateFollowers(org, level, thread_num, log_file_writer, follower_count_wr
   outptr.close()
 
   # Semaphore
-  lock.acquire()
-  file_queue.put(level, "followers_" + org + ".txt")
-  lock.release()
+  if(level < max_level):
+    lock.acquire()
+    file_queue.put((level, "followers_" + org + ".txt"))
+    lock.release()
 
   return num_scraped_followers
 
@@ -223,6 +231,12 @@ def make_directory(name):
 
 def next_follower(reader):
   return next(reader)[0]
+
+def is_somethread_alive():
+  for thread_num in range(max_threads):
+    if(threads[thread_num] != None and threads[thread_num].isAlive()):
+      return True
+  return False
 
 if __name__=="__main__":
   main()
